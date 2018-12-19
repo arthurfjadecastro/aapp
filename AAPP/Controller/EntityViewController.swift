aa60ad85bb32d33e9a46d4e7f40b71dced76cc42
Arthur Franco Junqueira Araujo de Castro <arthur_ravin@hotmail.com>
2018-12-19 16:06:55 -0200

    initial setting

---
+++ AAPP/Controller/EntityViewController.swift
@@ -0,0 +1,12 @@
+//
+//  EntityViewController.swift
+//  AAPP
+//
+//  Created by Arthur Castro on 19/12/2018.
+//  Copyright © 2018 Arthur Castro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EntityViewController: UIViewController {}
